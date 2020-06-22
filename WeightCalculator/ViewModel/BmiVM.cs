852d0a041117d21ddc5e709e462851f1c152659a
PieterQQ <pieter444@wp.pl>
2020-06-22 12:30:45 +0200

    English Bmi

--- WeightCalculator/ViewModel/BmiVM.cs
+++ WeightCalculator/ViewModel/BmiVM.cs
@@ -28,23 +28,39 @@ namespace WeightCalculator.ViewModel
         {
             var currentBMI = new BetterReturn();
             currentBMI.Value  = Math.Round((weight / (Math.Pow((double.Parse(Height.ToString()) / 100), 2))),2);
-            if (currentBMI.Value < 18.5)
+            if (currentBMI.Value < 15)
             {
-                currentBMI.Text = "Niedowaga";
+                currentBMI.Text = "Very severely underweight";
             }
-            else if (currentBMI.Value <= 24.9)
+           else if (currentBMI.Value < 16)
             {
-                currentBMI.Text = "Waga prawidłowa";
+                currentBMI.Text = "Severely underweight";
             }
-            else if (currentBMI.Value <= 29.9)
+            else if(currentBMI.Value <= 18.5)
             {
-                currentBMI.Text = "Nadwaga";
+                currentBMI.Text = "Underweight";
             }
-            else if (currentBMI.Value > 29.9)
+            else if (currentBMI.Value < 25)
             {
-                currentBMI.Text = "Otyłość";
+                currentBMI.Text = "Normal";
             }
-          
+            else if (currentBMI.Value < 30)
+            {
+                currentBMI.Text = "Overweight";
+            }
+            else if (currentBMI.Value < 35)
+            {
+                currentBMI.Text = "Moderately obese";
+            }
+            else if (currentBMI.Value <= 40)
+            {
+                currentBMI.Text = "Severely obese";
+            }
+            else if (currentBMI.Value > 40)
+            {
+                currentBMI.Text = "Very severely obese";
+            }
+
             return currentBMI;
         }
        
