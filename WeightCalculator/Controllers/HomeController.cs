b74fa04cf2a7aa3d03a8706d0c32d8a4570b2162
PieterQQ <pieter444@wp.pl>
2020-06-22 14:01:14 +0200

    Simple Validation for now

--- WeightCalculator/Controllers/HomeController.cs
+++ WeightCalculator/Controllers/HomeController.cs
@@ -23,6 +23,30 @@ namespace WeightCalculator.Controllers
                 StartDate=BMI.StartDate,
                 EndDate=BMI.EndDate
             };
+            if (ModelState.IsValidField("StartDate") && ModelState.IsValidField("EndDate"))
+            {  if (Model.StartDate.Date < DateTime.Now.Date || Model.EndDate.Date < DateTime.Now.Date)
+                {
+                    ModelState.AddModelError("StartDate", "Back To the future?");
+                }
+                else if (Model.StartDate.Date >= Model.EndDate.Date)
+                {
+                    ModelState.AddModelError("StartDate", "Start date should be lower than End Date");
+                }
+               
+             
+            }
+            if (ModelState.IsValidField("InitWeight") && ModelState.IsValidField("DesiredWeight") && Model.InitWeight == Model.DesiredWeight)
+            {
+                ModelState.AddModelError("InitWeight", "Initial Weight and Desired Weight cant be same");
+            }
+            if (!ModelState.IsValid)
+            {
+                var errorList = (from item in ModelState
+                                 where item.Value.Errors.Any()
+                                 select item.Value.Errors[0].ErrorMessage).ToList();
+                return Content(errorList.First().ToString());
+            }
+            else
             return PartialView("_ResultView",Model);
         }
 
--- WeightCalculator/ViewModel/BmiVM.cs
+++ WeightCalculator/ViewModel/BmiVM.cs
@@ -8,6 +8,7 @@ namespace WeightCalculator.ViewModel
     public class BmiVM
     {
         public int Height { get; set; }
+        
         public int InitWeight { get; set; }
         public int DesiredWeight { get; set; }
         public DateTime StartDate { get; set; }
